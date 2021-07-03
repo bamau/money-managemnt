40f3d67a973ed7e69727ee39b4e767a40430ec5f
BaMau <bamau97@gmail.com>
2021-07-03 16:23:48 +0700

    Break folder of project.

---
+++ ios/Pods/Target Support Files/Pods-MoneyManagement-MoneyManagementTests/Pods-MoneyManagement-MoneyManagementTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoneyManagement_MoneyManagementTests : NSObject
+@end
+@implementation PodsDummy_Pods_MoneyManagement_MoneyManagementTests
+@end
